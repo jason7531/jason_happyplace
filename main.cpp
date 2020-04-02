d87ce61d70ae8525614891e0827fd45944eb2436
jason7531 <54910136+jason7531@users.noreply.github.com>
2020-04-02 13:06:54 +0530

    Add files via upload
    c++ section9

--- main.cpp
+++ main.cpp
@@ -1,65 +1,78 @@
 #include <iostream>
-#include <string>
+#include <vector>
 using namespace std;
 int main()
 {
-	cout << "Welcome to Jason's Secret message service\n";
-    cout << "Please enter your message to be encrypted\n";
-    string encrypted_msg{};
-    string msg;
-    getline(cin,msg);
-    string dec{"ZXCVBNMASDFGHJKLQWERTYUIOPzxcvbnmasdfghjklqwertyuiop"};
-    string enc{"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-    for(char c:msg)
-    {
-        
-        size_t pos=enc.find(c);
-        if(pos!=string::npos) {
-        char new_char=dec.at(pos);
-        encrypted_msg+=new_char;
-        }
-        else {
-            encrypted_msg+=c;
-        }
-        
-    }
-//    for(size_t i=0;i<msg.length();++i) {
-//        for (size_t j=0;j<dec.length();++j) {
-//            if (msg[i]==enc[j]) 
-//                msg[i]=dec[j];
-//        }
-//    }
-    cout << "The encrypted message is " << encrypted_msg << endl;
-    cout << "Do you want to decrypt the message: (Y/N)\n";
-    char choice{'a'};
+    char choice {};
+     vector <int> vec{1,2,3};
+    do {
+   
+	cout << "Welcome to Jason's Menu\n" ;
+    cout << "The default number collection is {1,2,3}, the further operations can be done with the help of the given menu";
+    cout << "Choose any of the following options:\n" ;
+    cout << " P: Print numbers\n A:Add numbers to the collection \n M:Find mean of the numbers\n S:Find the smallest \n L:Find the largest\n Q:Quit\n" ;
+    
     cin >> choice;
-    string decrypt_msg{};
-    if (choice=='Y'||choice=='y') {
-//         for(size_t i=0;i<msg.length();++i) {
-//        for (size_t j=0;j<dec.length();++j) {
-//            if (msg[i]==dec[j]) 
-//                msg[i]=enc[j];
-//        }
-//    }
- for(char c:encrypted_msg)
-    {
+    if (choice=='p'||choice=='P') {
+        if(vec.size()==0)
+            cout << "The list is empty";
+            else {
+                 cout<<"THe numbers are [ " ;
+        for(auto val:vec)
+           cout << val << " ";
+           cout << "]" << endl;
+            }
         
-        size_t pos=dec.find(c);
-        if(pos!=string::npos) {
-        char new_char=enc.at(pos);
-        decrypt_msg+=new_char;
+    }
+    
+            
+        else if(choice=='a'||choice=='A') {
+            int num{};
+            cout << "Enter the numbers to be added to the collection" << endl;
+            cin >> num;
+            vec.push_back(num);
         }
-        else {
-            decrypt_msg+=c;
-        }
-        
+            else if(choice=='m'||choice=='M')
+            {
+                int sum {0};
+                double mean;
+                 for(size_t i=0;i<vec.size();++i)
+                 {
+                     sum+=vec.at(i);
+                 }
+                 mean=static_cast<double>(sum)/vec.size();
+                 cout << "The mean of the numbers is " << mean << endl;
+                 }
+                     
+                else if(choice=='s'||choice=='S') {
+                    int small {0};
+                     for(size_t i=0;i<vec.size();++i)
+                     {
+                          small=vec.at(0);
+                         if(vec.at(i)<=small)
+                             small=vec.at(i);
+                       
+                     }
+                     cout << "The smallest numbers is " << small << endl;
+                }
+                             
+                    else if(choice=='l'||choice=='L') {
+                        int big {0};
+                     for(size_t i=0;i<vec.size();++i)
+                     {
+                          big=vec.at(0);
+                         if(vec.at(i)>=big)
+                             big=vec.at(i);
+                       
+                     }
+                     cout << "The largest number is " << big << endl;
+                    }
+                        else if(choice=='q'||choice=='Q')
+                            cout << "Thanks for using Jason Menu";
+                            else {
+                                cout << "Invalid Choice PLease try again" << endl;
+                            }
     }
-
-    cout << "The decrypted message is " << decrypt_msg << endl;
-    }
-    else {
-       cout << "Thanks for using Jason messaging service\n";
-        
-    }
+    while (choice!='q' && choice!='Q');
 	return 0;
 }

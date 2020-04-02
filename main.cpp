f7e9cccc5fd62fde95b96f4bcb75f4bb9d31f260
jason7531 <54910136+jason7531@users.noreply.github.com>
2020-04-02 13:08:55 +0530

    Add files via upload
    c++ section6

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
-	cout << "Enter amounts of cents: ";
-    int cent;
-    cin >> cent;
-    cout << "conversion can be done as :\n" << "Dollars: " << cent/100 << endl << "Quarters: " << (cent%100)/25 << endl << "Dime: " << ((cent%100)%25)/10 << endl <<"Nickels: "<< (((cent%100)%25)%10)/5 << endl << "Pennies: " << ((((cent%100)%25)%10)%5) << endl;
+	vector<int> vector_1;
+    vector<int> vector_2;
+    
+    vector_1.push_back(10);
+    vector_1.push_back(20);
+    cout << "Vector 1 has " << vector_1.at(0) << " " << vector_1.at(1) << " and contains " << vector_1.size() << " elements\n";
+    vector_2.push_back(100);
+    vector_2.push_back(200);
+     cout << "Vector 2 has " << vector_2.at(0) << " " << vector_2.at(1) << " and contains " << vector_2.size() << " elements\n";
+     
+     vector<vector<int>> vector_2d;
+    
+   
+     vector_2d.push_back(vector_1);
+     vector_2d.push_back(vector_2);
+     
+     
+     
+     cout << "2d vector has " << vector_2d.at(0).at(0) << " "<<vector_2d.at(0).at(1) << endl;
+     cout << vector_2d.at(1).at(0) << " " << vector_2d.at(1).at(1) << endl;
+     
+     vector_1.at(0)=1000;
+     cout << "2d vector has " << vector_2d.at(0).at(0) << " "<<vector_2d.at(0).at(1) << endl;
+     cout << vector_2d.at(1).at(0) << " " << vector_2d.at(1).at(1) << endl;
+       cout << "Vector 1 has " << vector_1.at(0) << " " << vector_1.at(1) << " and contains " << vector_1.size() << " elements\n";
+     
+     
+     
     
     
 	return 0;
